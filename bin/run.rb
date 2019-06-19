bccf93d626fc65b8cb1185813123a8cbb4141ee7
github username <github email address>
2019-06-19 10:07:50 -0400

    Minor changes to welcome message

--- bin/run.rb
+++ bin/run.rb
@@ -6,12 +6,12 @@ ActiveRecord::Base.logger = nil
 
 def welcome_message
   puts "----------------------------------------------------------------"
-  puts "Welcome to our app. You can view either playlists or create one."
+  puts "Welcome to our app. You can: View playlists or Create one ..."
 end
 
 def welcome
   puts "----------------------------------------------------------------"
-  puts "Create||View||Edit"
+  puts "|Create||View||Edit|"
   puts "\n"
   input = gets.chomp.downcase
   if input == "create"
